74c67263e91b4ef08f9a0ab356af702088224d42
aymen.ferjani <aymen.ferjani.zizou90@gmail.com>
2024-05-10 18:47:00 +0100

    Test - 3 - copyleaks

--- plagiarismCheck/routes.js
+++ plagiarismCheck/routes.js
@@ -36,4 +36,15 @@ router.get('/creditsChecked/:scanID', (req, res, next) => {
   });
 });
 
+router.get('/indexed/:scanID', (req, res, next) => {
+  const scanID = req.params.scanID;
+
+  console.log(`The 'indexed' route is fired!`);
+  console.log(`Got scanID = ${scanID}`);
+  console.log(`req.body = ${req.body}`);
+
+  return res.status(200).json({
+  });
+});
+
 module.exports = router;
